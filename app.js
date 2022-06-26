00cd24fdf5074b478f702599ec339575af306282
Marcus <vini.mark2002@gmail.com>
2022-06-26 13:22:33 -0300

    Aula - Rota POST

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const checkListRouter = require('./src/routes/checklist');
 const app = express();
 app.use( express.json() );
 
-app.use('/', checkListRouter);
+app.use('/checklists', checkListRouter);
 
 app.listen(3000, () => {
     console.log('Servidor foi iniciado');
