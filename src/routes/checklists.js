c51675463256f04c1777c0a0e288068167cdf730
Marcus <vini.mark2002@gmail.com>
2022-07-08 01:01:11 -0300

    Aula - Rota POST

--- src/routes/checklists.js
+++ src/routes/checklists.js
@@ -3,8 +3,13 @@ const express =  require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('Olá')
-    req.send();
+    console.log('Olá');
+    res.send();
 });
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).json(req.body);
+})
 
 module.exports = router;
