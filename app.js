fb5ba53dd3b555ff264f3f2f1bc3cdb1d11b8192
Marcus <vini.mark2002@gmail.com>
2022-07-08 00:25:15 -0300

    Aula - Middlewares

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const log = (req, res, next) => {
+    console.log(req.body);
+    console.log(`Data: ${Date.now()}`);
+    next();
+}
+
+app.use(log);
+
 app.get('/', (req, res) => {
     res.send('<h1>Minha lista de tarefas</h1>');
 })
