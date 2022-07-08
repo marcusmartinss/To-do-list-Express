b85efef87becf4bf302ab870248aa3d04f23a0aa
Marcus <vini.mark2002@gmail.com>
2022-07-08 01:12:20 -0300

    Aula - Parâmetros nas rotas

--- src/routes/checklists.js
+++ src/routes/checklists.js
@@ -14,4 +14,15 @@ router.post('/', (req, res) => {
     // responde com o status 200 e o json do body
 })
 
+router.get('/:id', (req, res) => {
+    // dois pontos indica que um parametro 'id' esta sendo esperado
+
+    console.log(req.params.id);
+    // automaticamente o express pega o que vem em :id e
+    // preenche o objeto colocando em id
+    
+    res.send(`ID: ${req.params.id}`);
+    // responde com o id
+});
+
 module.exports = router;
