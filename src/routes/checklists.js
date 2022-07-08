f32e30128cfba0a8a8e44e8308d5108b602b73b8
Marcus <vini.mark2002@gmail.com>
2022-07-08 01:03:12 -0300

    Adicionados commentários (Aula - Rota POST)

--- src/routes/checklists.js
+++ src/routes/checklists.js
@@ -4,12 +4,14 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     console.log('Olá');
-    res.send();
+    res.send(); 
+    // responde com o que foi enviado
 });
 
 router.post('/', (req, res) => {
     console.log(req.body);
     res.status(200).json(req.body);
+    // responde com o status 200 e o json do body
 })
 
 module.exports = router;
